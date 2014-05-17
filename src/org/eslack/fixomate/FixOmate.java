9ca1c43051e4dc75c766456cb1487ad8cec19748
Pau Oliva <pau@eslack.org>
2014-05-17 15:39:21 +0200

    initial commit

---
+++ src/org/eslack/fixomate/FixOmate.java
@@ -0,0 +1,24 @@
+package org.eslack.fixomate;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+public class FixOmate implements IXposedHookLoadPackage {
+    public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
+        if (!lpparam.packageName.equals("android.view.MotionEvent"))
+            return;
+
+        findAndHookMethod("android.view.MotionEvent", lpparam.classLoader, "getPointerCount", new XC_MethodReplacement() {
+            @Override
+		protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+			//XposedBridge.log("PAU: Hook in place");
+			return 1;
+		}
+        });
+
+    }
+}
